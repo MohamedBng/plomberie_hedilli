c21786dffff9d05dab07f8747392a7e0dba90adc
Mohamed Bengrich <77544040+MohamedBng@users.noreply.github.com>
2022-04-10 17:50:05 +0200

    Add files via upload

---
+++ php/deleteGalerie.php
@@ -0,0 +1,16 @@
+<?php
+     require_once '../php/bdd.php';
+
+if(isset($_GET['id'])){
+  // récupérer les valeurs 
+  $id = $_GET["id"];
+
+  // requete sql pour supprimer 
+  $req = $pdo->query('DELETE FROM galerie WHERE id='.$id.'');
+
+  // Redirection vers la page d'administration
+  header('Location: ../admin.php');
+
+  
+}
+?>
